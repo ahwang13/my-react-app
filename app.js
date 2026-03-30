e167eb3cff57154d238a1e69d4bb46ad67b9804f
ahwang13 <ahwang@wustl.edu>
2026-03-30 01:28:04 -0500

    Update app.js - add calculate winner to main game

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   
   function handleClick(i){
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)){
       return;
     }
     
@@ -28,9 +28,18 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
   
   return (
     <>
+      <div className = "status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick = {() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick = {() => handleClick(1)} />
